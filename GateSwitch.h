d93f7c442a7735e267f1bf45da63c00dfc394ee4
蔡斌卿 <caibinqing@live.com>
2022-11-13 00:32:45 +0800

    esp32

--- GateSwitch.h
+++ GateSwitch.h
@@ -1,16 +1,29 @@
 #include "esphome.h"
 
+// esp32
+#define LED_ON HIGH
+#define LED_OFF LOW
+// // esp8266
+// #define LED_ON LOW
+// #define LED_OFF HIGH
+
 class GateSwitch : public Component, public Switch {
 public:
-  int phone = 5;
-  int lock = 4;
-  int led = 16;
+  // esp32
+  int phone = 25;
+  int lock = 26;
+  int led = 2;
+  // esp8266
+  // int phone = 5;
+  // int lock = 4;
+  // int led = 16;
+
 
   void setup() override {
     pinMode(phone, OUTPUT);
     pinMode(lock, OUTPUT);
     pinMode(led, OUTPUT);
-    digitalWrite(led, HIGH);
+    digitalWrite(led, LED_OFF);
   }
 
   void write_state(bool state) override {
@@ -20,16 +33,16 @@ public:
 
       // ESP_LOGD("gate_switch", "write_state START!");
 
-      digitalWrite(led, LOW);
+      digitalWrite(led, LED_ON);
       digitalWrite(phone, HIGH);
       delay(500);
-      digitalWrite(led, HIGH);
+      digitalWrite(led, LED_OFF);
       digitalWrite(lock, HIGH);
       delay(200);
-      digitalWrite(led, LOW);
+      digitalWrite(led, LED_ON);
       digitalWrite(lock, LOW);
       delay(500);
-      digitalWrite(led, HIGH);
+      digitalWrite(led, LED_OFF);
       digitalWrite(phone, LOW);
 
       // ESP_LOGD("gate_switch", "write_state DONE!");
